f6039b851ecbbbfb67122c7e9afa5ad46d9704b9
github username <crishanks12@gmail.com>
2019-06-15 17:22:24 -0600

    ul background hover styles and first block animation with jquery

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
+  //Block Styles
+  $('.block:first').slideUp(500).show(800);
+  $('#list').hover(function() {
+    $(this).css('background-color', 'lightblue');
+  }).mouseleave(function() {
+    $(this).css('background-color', 'white');
+  });
 });
